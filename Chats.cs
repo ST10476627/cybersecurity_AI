f84790b0df71336627728a6c6adcdf9e63f7db34
ST10476627 <ST10476627@rcconnect.edu.za>
2026-04-13 10:31:51 +0200

    Add files via upload

---
+++ Chats.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ai_chats
+{
+    public class Chats
+    {
+        public void StartChat()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("What can I help you with? or (type 'exit' to quit)");
+
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write("user: ");
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "exit")
+                {
+                   Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine("LUMI:Goodbye thanks for using Lumispark your spark of bright ideas");
+                    break;
+                }
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("LUMI: What can I assist you with.");
+                    continue;
+                }
+
+                ResponseClass.Respond(input);
+            }
+        }
+    }
+}
